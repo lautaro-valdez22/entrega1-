178265932bb1a363f7164f453422cacd12cbc502
LeandroFrattini <leo.frattini@gmail.com>
2022-06-05 18:09:20 -0300

    cambios url,admin,views

--- ENTREGA1/product/admin.py
+++ ENTREGA1/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from product.models import Celulares, Heladeras, Televisores
+
 # Register your models here.
+admin.site.register(Celulares)
+admin.site.register(Heladeras)
+admin.site.register(Televisores)
