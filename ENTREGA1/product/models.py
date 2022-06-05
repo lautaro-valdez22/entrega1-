bd3ea7f687c3501b3b853bde8a1ad5f339a1b936
LeandroFrattini <leo.frattini@gmail.com>
2022-06-05 16:31:07 -0300

    commitleandro

--- ENTREGA1/product/models.py
+++ ENTREGA1/product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Celulares(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, blank=True, null=True)
+    SKU = models.CharField(max_length=30, unique=True)
+    active = models.BooleanField(default=True)
+    
+    
+class Heladeras(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, blank=True, null=True)
+    SKU = models.CharField(max_length=30, unique=True)
+    active = models.BooleanField(default=True)
+    
+    
+class Televisores(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, blank=True, null=True)
+    SKU = models.CharField(max_length=30, unique=True)
+    active = models.BooleanField(default=True)
