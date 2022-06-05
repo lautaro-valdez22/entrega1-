8039cd7222d2e854f6922f2174ce0479b099f68c
LeandroFrattini <leo.frattini@gmail.com>
2022-06-05 18:44:54 -0300

    commit con algunas vistas

--- ENTREGA1/product/views.py
+++ ENTREGA1/product/views.py
@@ -1,4 +1,19 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from product.models import Celulares, Heladeras, Televisores
+from product.forms import Celulares_form, Heladeras_form, Televisores_form
+
 # Create your views here.
+def products(request):
+    print(request.method)
+    productos = Celulares.objects.all()
+    context = {'celulares':Celulares}
+    return render(request, 'celulares.html', context=context)
+
+def products(request):
+    print(request.method)
+    productos = Heladeras.objects.all()
+    context = {'heladeras': Heladeras}
+    return render(request, 'heladeras.html', context=context)
