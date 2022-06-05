e52adb15ea24d1d4e6c654a4c42dd34ec3da82f5
LeandroFrattini <leo.frattini@gmail.com>
2022-06-05 16:40:38 -0300

    3er commit

--- ENTREGA1/product/models.py
+++ ENTREGA1/product/models.py
@@ -22,3 +22,5 @@ class Televisores(models.Model):
     description = models.CharField(max_length=200, blank=True, null=True)
     SKU = models.CharField(max_length=30, unique=True)
     active = models.BooleanField(default=True)
+    
+    Prueba
